70c21dddcbb77ab30c25ad6a3335dd06d27835e2
carlos <carlosarroyave991@gmail.com>
2021-03-07 15:16:08 -0500

    primer comentario

---
+++ peliculas-gratis-backend/src/main/java/com/otakutv/peliculasgratis/PeliculasGratisApplication.java
@@ -0,0 +1,13 @@
+package com.otakutv.peliculasgratis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliculasGratisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliculasGratisApplication.class, args);
+	}
+
+}
